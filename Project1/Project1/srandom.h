96169f44b12940c060c985cc1678f834806c5ab2
orargaman <orargaman@gmail.com>
2016-06-07 17:34:08 +0300

    t

--- Project1/Project1/srandom.h
+++ Project1/Project1/srandom.h
@@ -1,1 +1,7 @@
 #pragma once
+
+#include <linux/module.h>
+#include <linux/kernel.h> /* for using printk */
+MODULE_LICENSE("GPL");
+ssize_t read(struct file *filp, char *buff, size_t	count, loff_t *offp);
+ssize_t write(struct file *filp, const char *buff,	size_t count, loff_t *offp);
