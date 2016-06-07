a2e9ea44f8902e8965fbd604b2765d19e8c1a267
orargaman <orargaman@gmail.com>
2016-06-07 19:52:07 +0300

    check

--- srandom.c
+++ srandom.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 #include "srandom.h"
 
-static struct file_operations fops = {
+/*static struct file_operations fops = {
 	.read = device_read,
 	.write = device_write,
 	.open = device_open,
 	.release = device_release
-};
+};*/
 
 int init_module(void)
 {
-	SET_MODULE_OWNER(&fops);
+	//SET_MODULE_OWNER(&fops);
 	printk("Hello, World\n");
-	register_chrdev(62,"srandom",&fops);
+	register_chrdev(62,"srandom",NULL);
 	return 0;
 }
 void cleanup_module(void)
@@ -21,9 +21,9 @@ void cleanup_module(void)
 	printk("Goodbye cruel world\n");
 }
 
-static int device_open(struct inode *, struct file *) { return 0; }
+/*static int device_open(struct inode *, struct file *) { return 0; }
 static int device_release(struct inode *, struct file *) {
 	return 0;
 }
 static ssize_t device_read(struct file *, char *, size_t, loff_t *) { return 0; }
-static ssize_t device_write(struct file *, const char *, size_t, loff_t *) { return 0; }
+static ssize_t device_write(struct file *, const char *, size_t, loff_t *) { return 0; }*/
