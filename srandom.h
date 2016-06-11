a480903af26ddf53e449304fd908fb03fb344bed
orargaman <orargaman@gmail.com>
2016-06-11 16:06:38 +0300

    can compile

--- srandom.h
+++ srandom.h
@@ -1,6 +1,13 @@
 #ifndef _MY_MODULE_H_
 #define _MY_MODULE_H_
 
+#define SRANDOM_MAGIC 'r'
+static ssize_t device_read(struct file *, char * buffer, size_t n, loff_t *);
+static ssize_t device_write(struct file *, const char* buffer, size_t n, loff_t *);
+int ioctl(struct inode *inode, struct file *filp, unsigned int cmd, unsigned long arg);
+int init_module(void);
+void cleanup_module(void);
+
 #include <linux/ioctl.h>
 
 
